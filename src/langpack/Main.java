3e507cf708321a4dc2ab2619bf10cc7b9e2a93da
Muhammad Abdullah <muabdullah@nisum.com>
2026-03-08 15:32:04 +0500

    After updation of readibility

--- src/langpack/Main.java
+++ src/langpack/Main.java
@@ -6,23 +6,29 @@ public class Main {
         String string1 = "Hello";
         String string2 = "World";
         //checking String references
-        System.out.println(string1 == string2);
+        System.out.println("In String1 " + string1 + " In String2 " + string2);
+        System.out.println("Checking reference using the == operator on String1 and String2: " + (string1 == string2));
 
         String string3 = "Hello";
         String string4 = "Hello";
-        System.out.println("\n" + (string3 == string4));
+        System.out.println("\nIn String3 " + string3 + " In String4 " + string4);
+        System.out.println("Checking reference using the == operator on String3 and String4: " + (string3 == string4));
 
-        System.out.println("\n" + (string1.equals(string2)));
-        System.out.println("\n" + (string3.equals(string4)));
+        System.out.println("\nIn String1 " + string1 + " In String2 " + string2);
+        System.out.println("Checking Using Equal Method: " + (string1.equals(string2)));
+        System.out.println("\nIn String3 " + string3 + " In String4 " + string4);
+        System.out.println("Checking Using Equal Method: " + (string3.equals(string4)));
 
         String string5 = new String("Hello");
         String string6 = new String("Hello");
 
         // check using objects its return false because reference of both is different
         //its return false because both have different object
-        System.out.println("\n" + (string5 == string6));
+        System.out.println("\nIn String1 " + string5 + " In String2 " + string6);
+        System.out.println("Create Same String Content But In Different Object Then Check Using == Operator: " + ((string5 == string6)));
         //its return true because its check content not the reference
-        System.out.println("\n" + (string5.equals(string6)));
+        System.out.println("\nIn String1 " + string5 + " In String2 " + string6);
+        System.out.println("Create Same String Content But In Different Object Then Check Using Equal Method: " + (string5.equals(string6)));
 
     }
 
