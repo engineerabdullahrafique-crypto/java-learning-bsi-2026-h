1f322614201635b2dd5b7a300818d463b152751f
Muhammad Abdullah <muabdullah@nisum.com>
2026-03-10 21:02:19 +0500

    Custom null String exception

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public void checkString(String stringToCheck) throws NullStringException {
+        if (stringToCheck == null) {
+            System.out.println("\nNull String Is Not Allowed");
+            throw new NullStringException("Null String Exception Thrown");
+        }
+        System.out.println("String Is: " + stringToCheck);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        try {
+            main.checkString(null);
+        } catch (NullStringException e) {
+            System.out.println(e);
+        }
+    }
+
+}
